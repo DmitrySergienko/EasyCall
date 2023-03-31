05f464ce3e404408334fc8b91c76943de0beaeae
DmitrySergienko <dmitrisergienko@gmail.com>
2023-03-31 12:14:14 +0300

    Glance widget attached

---
+++ app/src/main/java/com/easycall/widgets/FirstWidget.kt
@@ -0,0 +1,83 @@
+package com.easycall.widgets
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.widget.RemoteViews
+import androidx.core.content.ContextCompat
+import com.easycall.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+private const val MY_ACTION = "MY_ACTION"
+
+class FirstWidget : AppWidgetProvider() {
+
+    private val scope = CoroutineScope(Dispatchers.IO)
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+
+        if (intent.action != MY_ACTION) return
+
+        val appWidgetManager = AppWidgetManager.getInstance(context)
+        val appWidgetId = ComponentName(context, FirstWidget::class.java)
+
+        RemoteViews(
+            context.packageName,
+            R.layout.initial_layout_widget
+        ).apply {
+            scope.launch {
+
+                makeCall(context, "")
+
+                //update widget manager
+                appWidgetManager.updateAppWidget(appWidgetId, this@apply)
+
+            }
+        }
+    }
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        //отправляю intent чтобы активировать метод OnReceive, (для этого использую pending intent)
+        val intent = Intent(context, FirstWidget::class.java).apply {
+            action = MY_ACTION
+        }
+        val pendingIntent =
+            PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        appWidgetIds.forEach { appWidgetId ->
+            RemoteViews(
+                context.packageName,
+                R.layout.initial_layout_widget
+            ).apply {
+                scope.launch {
+
+                    //set pending intent
+                    setOnClickPendingIntent(R.id.widget_root, pendingIntent)
+
+                    appWidgetManager.updateAppWidget(appWidgetId, this@apply)
+                }
+            }
+        }
+
+    }
+
+
+    private fun makeCall(context: Context, phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:$phoneNumber"))
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        ContextCompat.startActivity(context, intent, null)
+    }
+
+}
