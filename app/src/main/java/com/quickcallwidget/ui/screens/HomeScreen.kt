a8d5f04d2ca34e441df5ed77a51b6ac6b026ab85
DmitrySergienko <dmitrisergienko@gmail.com>
2023-05-17 14:21:04 +0300

    3 widgets can be add

--- app/src/main/java/com/quickcallwidget/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/quickcallwidget/ui/screens/HomeScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -26,10 +25,8 @@ import com.quickcallwidget.data.db.MyDao
 import com.quickcallwidget.data.db.TestDB
 import com.quickcallwidget.ui.navigation.Screen
 import com.quickcallwidget.ui.screens.utils.CycleButtonWithPlus
+import com.quickcallwidget.ui.screens.utils.DeleteWidget
 import com.quickcallwidget.ui.screens.utils.fontFamily
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(
@@ -74,7 +71,6 @@ fun HomeScreen(
                 if (sharedPrefs.getInt("WidgetDeleted", 0) != 0) {
 
                     when (sharedPrefs.getInt("WidgetDeleted", 0)) {
-
                         10 ->{
                             Toast.makeText(context, "Delete widget 1", Toast.LENGTH_SHORT).show()
                             DeleteWidget(
@@ -83,7 +79,8 @@ fun HomeScreen(
                                 list = list,
                                 wName = "Name",
                                 wPhone = "Phone",
-                                wNumber = 1
+                                wNumber = 1,
+                                deletedWidget = 1
                             )}
                         20 ->{
                             Toast.makeText(context, "Delete widget 2", Toast.LENGTH_SHORT).show()
@@ -95,6 +92,7 @@ fun HomeScreen(
                                     wName = "NameTwo",
                                     wPhone = "PhoneTwo",
                                     wNumber = 2,
+                                    deletedWidget = 2
                                     )}else{
                                 DeleteWidget(
                                     navController = navController,
@@ -103,6 +101,7 @@ fun HomeScreen(
                                     wName = "NameTwo",
                                     wPhone = "PhoneTwo",
                                     wNumber = 1,
+                                    deletedWidget = 2
                                     )}
                             }
                         30 ->{
@@ -115,6 +114,7 @@ fun HomeScreen(
                                         wName = "NameThree",
                                         wPhone = "PhoneThree",
                                         wNumber = 1,
+                                        deletedWidget = 3
                                     )}
                                 2->{Toast.makeText(context, "Delete widget 3", Toast.LENGTH_SHORT).show()
                                     DeleteWidget(
@@ -124,6 +124,7 @@ fun HomeScreen(
                                         wName = "NameThree",
                                         wPhone = "PhoneThree",
                                         wNumber = 2,
+                                        deletedWidget = 3
                                     )}
                                 3->{Toast.makeText(context, "Delete widget 3", Toast.LENGTH_SHORT).show()
                                     DeleteWidget(
@@ -133,6 +134,7 @@ fun HomeScreen(
                                         wName = "NameThree",
                                         wPhone = "PhoneThree",
                                         wNumber = 3,
+                                        deletedWidget = 3
                                     )}
                             }
                         }
@@ -153,66 +155,29 @@ fun HomeScreen(
                         }
                     }
                 }
+                if (widNumber != 4) {
+                    CycleButtonWithPlus {
+                        when (widNumber) {
+                            1 -> navController.navigate(Screen.DetailsScreenOne.route)
+                            2 -> navController.navigate(Screen.DetailsScreenTwo.route)
+                            3 -> navController.navigate(Screen.DetailsScreenThree.route)
 
-                CycleButtonWithPlus {
-
-                    when (widNumber) {
-
-                        1 -> navController.navigate(Screen.DetailsScreenOne.route)
-                        2 -> navController.navigate(Screen.DetailsScreenTwo.route)
-                        3 -> navController.navigate(Screen.DetailsScreenThree.route)
-                        4 -> Toast.makeText(context, "Maximum 3 widgets created", Toast.LENGTH_SHORT).show()
-
-                        else -> {
-                            navController.navigate(Screen.DetailsScreenOne.route)
+                            else -> {
+                                navController.navigate(Screen.DetailsScreenOne.route)
+                            }
                         }
                     }
+                }else{
+                    Text(
+                        text = stringResource(id = R.string.max),
+                        color = Color.White,
+                        fontFamily = fontFamily,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Black,
+                        textAlign = TextAlign.Center
+                    )
                 }
             }
-        }
-    }
-}
-@OptIn(DelicateCoroutinesApi::class)
-@Composable
-fun DeleteWidget(
-    navController: NavController,
-    myDao: MyDao,
-    list: MutableList<TestDB>,
-    wName: String,
-    wPhone: String,
-    wNumber: Int
-){
-    val context = LocalContext.current
-    val sharedPrefs = context.getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-
-    //1. delete widget from the room
-    if (list.isNotEmpty()) {
-        // Log.d("VVV", "HomeScreen() list: $list")
-        val item = list.first()
-        LaunchedEffect(true) {
-            GlobalScope.launch {
-                myDao.deleteItem(item)
-            }
-        }
-        // 2. clean share preferences
-        val editor = sharedPrefs.edit()
-        editor.putString(wName, null)
-            .putString(wPhone, null)
-            .putInt("WidNumber", wNumber)
-            .putInt("WidgetDeleted", 0)
-        editor.apply()
-    }
-
-
-    LazyColumn(
-        contentPadding = PaddingValues(bottom = 20.dp),
-    )
-    {
-        items(list) { contact ->
-            WidgetItem(
-                contact = contact,
-                navController = navController
-            )
         }
     }
 }
---
+++ app/src/main/java/com/quickcallwidget/ui/screens/utils/DeleteWidget.kt
@@ -0,0 +1,72 @@
+package com.quickcallwidget.ui.screens.utils
+
+import android.content.Context
+import android.util.Log
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.quickcallwidget.data.db.MyDao
+import com.quickcallwidget.data.db.TestDB
+import com.quickcallwidget.ui.screens.WidgetItem
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+@OptIn(DelicateCoroutinesApi::class)
+@Composable
+fun DeleteWidget(
+    navController: NavController,
+    myDao: MyDao,
+    list: MutableList<TestDB>,
+    wName: String,
+    wPhone: String,
+    wNumber: Int,
+    deletedWidget: Int
+){
+    val context = LocalContext.current
+    val sharedPrefs = context.getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+
+    //1. delete widget from the room
+    if (list.isNotEmpty()) {
+        //  val coroutineScope = rememberCoroutineScope()
+
+        LaunchedEffect(true) {
+            GlobalScope.launch {
+
+                val findItemId = list.find { it.id == deletedWidget}
+                if (findItemId != null) {
+                    myDao.deleteItem(findItemId)
+                }
+
+                Log.d("VVV", " findByName = $findItemId")
+                Log.d("VVV", " first = ${list.first()}")
+                Log.d("VVV", " last = ${list.last()}")
+                Log.d("VVV", " list = $list")
+            }
+        }
+        // 2. clean share preferences
+        val editor = sharedPrefs.edit()
+        editor.putString(wName, null)
+            .putString(wPhone, null)
+            .putInt("WidNumber", wNumber)
+            .putInt("WidgetDeleted", 0)
+        editor.apply()
+    }
+
+    LazyColumn(
+        contentPadding = PaddingValues(bottom = 20.dp),
+    )
+    {
+        items(list) { contact ->
+            WidgetItem(
+                contact = contact,
+                navController = navController
+            )
+        }
+    }
+}
